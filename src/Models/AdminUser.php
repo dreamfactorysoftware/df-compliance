b82586d4978592019795f557a1526d58e69f0577
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-04-04 10:29:40 +0300

    Add EventHandler to handle UserCreatingEvent

--- src/Models/AdminUser.php
+++ src/Models/AdminUser.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace DreamFactory\Core\Compliance\Models;
+
+use DreamFactory\Core\Exceptions\ForbiddenException;
 use DreamFactory\Core\Models\AdminUser as CoreAdminUser;
 
 
@@ -14,5 +17,20 @@ class AdminUser extends CoreAdminUser
     public static function getAdminByEmail($email)
     {
         return self::whereEmail($email)->get()->toArray()[0];
+    }
+
+    /**
+     * Set given user as root.
+     *
+     * @param $user
+     * @return bool
+     */
+    public static function setRoot($user)
+    {
+        if (!$user->is_sys_admin) {
+            throw new ForbiddenException('Only admins can be root.');
+        } else {
+            return $user->is_root_admin = 1;
+        }
     }
 }

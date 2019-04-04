427e545d5e10dd147c031d190731ed258acc1a8e
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-04-04 10:29:40 +0300

    Add EventHandler to handle UserCreatingEvent

---
+++ src/Handlers/Events/EventHandler.php
@@ -0,0 +1,40 @@
+<?php
+namespace DreamFactory\Core\Compliance\Handlers\Events;
+
+use DreamFactory\Core\Compliance\Models\AdminUser;
+use DreamFactory\Core\Events\UserCreatingEvent;
+use DreamFactory\Core\Events\BaseUserEvent;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class EventHandler
+{
+    /**
+     * Register the listeners for the subscriber.
+     *
+     * @param  Dispatcher $events
+     */
+    public function subscribe($events)
+    {
+        $events->listen(
+            [
+                UserCreatingEvent::class,
+            ],
+            static::class . '@handleUserCreatingEvent'
+        );
+    }
+
+    /**
+     * Handle User creating events.
+     *
+     * @param BaseUserEvent $event
+     *
+     * @return void
+     */
+    public function handleUserCreatingEvent($event)
+    {
+        if (!AdminUser::adminExists()) {
+            $user = $event->user;
+            AdminUser::setRoot($user);
+        }
+    }
+}

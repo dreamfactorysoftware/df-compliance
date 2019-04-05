c16f0b7088256995551bd4cc4377e5a2af9ba000
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-04-05 16:23:27 +0300

    Add RootAdminMiddleware alias

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,7 +5,7 @@ namespace DreamFactory\Core\Compliance;
 use DreamFactory\Core\Compliance\Handlers\Events\EventHandler;
 use DreamFactory\Core\Compliance\Http\Middleware\AccessibleTabs;
 use DreamFactory\Core\Compliance\Http\Middleware\HandleRestrictedAdmin;
-use DreamFactory\Core\Compliance\Http\Middleware\RootAdmin;
+use DreamFactory\Core\Compliance\Http\Middleware\RootAdmin as RootAdminMiddleware;
 use Illuminate\Routing\Router;
 use Route;
 use Event;
@@ -34,12 +34,12 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         // the method name was changed in Laravel 5.4
         if (method_exists(Router::class, 'aliasMiddleware')) {
-            Route::aliasMiddleware('df.root_admin', RootAdmin::class);
+            Route::aliasMiddleware('df.root_admin', RootAdminMiddleware::class);
             Route::aliasMiddleware('df.handle_restricted_admin', HandleRestrictedAdmin::class);
             Route::aliasMiddleware('df.accessible_tabs', AccessibleTabs::class);
         } else {
             /** @noinspection PhpUndefinedMethodInspection */
-            Route::middleware('df.root_admin', RootAdmin::class);
+            Route::middleware('df.root_admin', RootAdminMiddleware::class);
             Route::middleware('df.handle_restricted_admin', HandleRestrictedAdmin::class);
             Route::middleware('df.accessible_tabs', AccessibleTabs::class);
         }

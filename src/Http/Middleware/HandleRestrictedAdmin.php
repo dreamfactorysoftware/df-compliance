a83ba960acc32ee982486254041b9026b5204c17
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-04-19 15:58:35 +0300

    Refactor licence check for root admin

--- src/Http/Middleware/HandleRestrictedAdmin.php
+++ src/Http/Middleware/HandleRestrictedAdmin.php
@@ -35,12 +35,9 @@ class HandleRestrictedAdmin
         $this->method = $request->getMethod();
         $this->payload = $request->input();
 
-        if ($this->isRestrictedAdminRequest()) {
-            if (!LicenseCheck::isGoldLicense() && AdminUser::isCurrentUserRootAdmin()) {
+        if ($this->isRestrictedAdminRequest() && AdminUser::isCurrentUserRootAdmin()) {
+            if (!LicenseCheck::isValidLicense()) {
                 throw new ForbiddenException('Restricted admins are not available for your license. Please upgrade to Gold.');
-            }
-            if (!AdminUser::isCurrentUserRootAdmin()) {
-                return $next($request);
             };
 
             $this->handleRestrictedAdminRequest();

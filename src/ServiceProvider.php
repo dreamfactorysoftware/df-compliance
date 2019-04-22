88c91fa2f39567447707add1fa2452009a1a0ee5
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-04-22 15:43:25 +0300

    Move handling of RA role to its own middleware

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,6 +6,7 @@ use DreamFactory\Core\Compliance\Commands\RootAdmin as RootAdminCommand;
 use DreamFactory\Core\Compliance\Handlers\Events\EventHandler;
 use DreamFactory\Core\Compliance\Http\Middleware\AccessibleTabs;
 use DreamFactory\Core\Compliance\Http\Middleware\HandleRestrictedAdmin;
+use DreamFactory\Core\Compliance\Http\Middleware\HandleRestrictedAdminRole;
 use DreamFactory\Core\Compliance\Http\Middleware\MarkAsRootAdmin;
 use Illuminate\Routing\Router;
 use Route;
@@ -39,16 +40,19 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
             Route::aliasMiddleware('df.mark_root_admin', MarkAsRootAdmin::class);
             Route::aliasMiddleware('df.handle_restricted_admin', HandleRestrictedAdmin::class);
             Route::aliasMiddleware('df.accessible_tabs', AccessibleTabs::class);
+            Route::aliasMiddleware('df.accessible_tabs', HandleRestrictedAdminRole::class);
         } else {
             /** @noinspection PhpUndefinedMethodInspection */
             Route::middleware('df.mark_root_admin', MarkAsRootAdmin::class);
             Route::middleware('df.handle_restricted_admin', HandleRestrictedAdmin::class);
             Route::middleware('df.accessible_tabs', AccessibleTabs::class);
+            Route::middleware('df.accessible_tabs', HandleRestrictedAdminRole::class);
         }
 
         Route::pushMiddlewareToGroup('df.api', 'df.mark_root_admin');
         Route::pushMiddlewareToGroup('df.api', 'df.handle_restricted_admin');
         Route::pushMiddlewareToGroup('df.api', 'df.accessible_tabs');
+        Route::pushMiddlewareToGroup('df.api', 'df.handle_restricted_admin_role');
     }
 
     /**
